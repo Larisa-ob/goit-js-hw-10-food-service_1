b5a7ccd36def4bff7b5f8d49c2c19fb0321f152d
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-12-09 23:35:00 +0200

    change index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './styles.css';
 import itemsElement from './menu.json';
 import itemsTemplate from './templates/menu.hbs';
-import './mymain';
+import './mymain.js';
+import './styles.css';
 const menu = document.querySelector('.js-menu');
 const makup = itemsTemplate(itemsElement);
 menu.insertAdjacentHTML('beforeend', makup);
