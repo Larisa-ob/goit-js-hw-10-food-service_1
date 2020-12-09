3cdc5df0d96d985f323628702a1b2f1e659f2abe
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-12-09 22:57:35 +0200

    change theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.css';
+import itemsElement from './menu.json';
+import itemsTemplate from './templates/menu.hbs';
+import './mymain';
+const menu = document.querySelector('.js-menu');
+const makup = itemsTemplate(itemsElement);
+menu.insertAdjacentHTML('beforeend', makup);
